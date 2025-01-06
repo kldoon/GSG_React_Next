d3dece7515523991e8bd3f92b3b3eb92ce25f603
Khaldoun Halawani <khalawani@sadasol.com>
2025-01-06 19:05:42 +0200

    Routeing 1 -Basic routing- (Student App 13)

--- 1_Students App/src/App.tsx
+++ 1_Students App/src/App.tsx
@@ -1,73 +1,27 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import './App.css'
-import { IStudent } from './types';
-
-import Student from './components/student/student.component';
-import AddForm from './components/add-form/add-form.component';
-import useLocalStorage from './hooks/local-storage.hook';
+import Main from './screens/Main.screen';
+import About from './screens/About.screen';
+import NotFound from './screens/NotFound.screen';
 
 function App() {
-  const [studentsList, setStudentsList] = useState<IStudent[]>([]);
-  const [totalAbsents, setTotalAbsents] = useState(0);
-  const lastStdRef = useRef<HTMLDivElement>(null);
-
-  const { storedData } = useLocalStorage(studentsList, 'students-list');
-
-  useEffect(() => {
-    const stdList: IStudent[] = storedData || [];
-    const totalAbs = stdList.reduce((prev, cur) => { return prev + cur.absents }, 0);
-    setTotalAbsents(totalAbs);
-    setStudentsList(stdList);
-  }, [storedData]);
-
-  const removeFirst = () => {
-    const newList = [...studentsList];
-    newList.shift();  // removes the first item
-    setStudentsList(newList);
-  }
-
-  const handleAbsentChange = (id: string, change: number) => {
-    setTotalAbsents(totalAbsents + change);
-    setStudentsList(studentsList.map(std => std.id === id ? { ...std, absents: std.absents + change } : std));
-  }
-
-  const handleAddStudent = (newStudent: IStudent) => {
-    setStudentsList([newStudent, ...studentsList]);
-  }
-
-  const scrollToLast = () => {
-    if (lastStdRef.current) {
-      lastStdRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  }
-
+  const [currentPage, setCurrentPage] = useState('main');
   const h1Style = { color: '#69247C', fontSize: '24px' };
 
   return (
     <div className="main wrapper">
       <h1 style={h1Style}>Welcome to GSG React/Next Course</h1>
-      <AddForm className="addForm" onSubmit={handleAddStudent} />
-      <div className='stats'>
-        <button onClick={removeFirst}>POP Student</button>
-        <button onClick={scrollToLast}>Scroll to Last</button>
-        <b style={{ fontSize: '12px', fontWeight: 100, color: 'gray' }}>Total Absents {totalAbsents}</b>
-      </div>
+      <nav>
+        <button onClick={() => setCurrentPage('main')}>Home Page</button>
+        <button onClick={() => setCurrentPage('about')}>About App</button>
+      </nav>
       {
-        studentsList.map(student => (
-          <Student
-            key={student.id}
-            id={student.id}
-            name={student.name}
-            age={student.age}
-            absents={student.absents}
-            isGraduated={student.isGraduated}
-            coursesList={student.coursesList}
-            onAbsentChange={handleAbsentChange}
-          />
-        )
-        )
+        currentPage === 'main'
+          ? <Main />
+          : currentPage === 'about'
+            ? <About />
+            : <NotFound />
       }
-      <div ref={lastStdRef}></div>
     </div>
   )
 }
---
+++ 1_Students App/src/screens/About.screen.tsx
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <>
+      <h2>About Students App</h2>
+      <p>
+        This is an app that we use to explain topics of react in GSG training.<br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui corrupti optio omnis soluta nam quas accusamus magni. Ipsa quos ipsam quae voluptas tempore quaerat, quidem veniam illum, eius autem cumque?
+      </p>
+      <div style={{ minHeight: '80vh', maxWidth: '600px' }}></div>
+    </>
+  )
+}
+
+export default About;
