9b5a6302707c6452174f4a4571a7dd1d6f0afd11
Khaldoun Halawani <khalawani@sadasol.com>
2025-01-06 20:02:54 +0200

    Routeing 3 react-router-dom (Student App 15)

--- 1_Students App/src/App.tsx
+++ 1_Students App/src/App.tsx
@@ -2,25 +2,25 @@ import './App.css'
 import Main from './screens/Main.screen';
 import About from './screens/About.screen';
 import NotFound from './screens/NotFound.screen';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   const h1Style = { color: '#69247C', fontSize: '24px' };
-  const currentPage = window.location.pathname;
 
   return (
     <div className="main wrapper">
       <h1 style={h1Style}>Welcome to GSG React/Next Course</h1>
-      <nav>
-        <a href='/'>Home Page</a>
-        <a href='/about'>About App</a>
-      </nav>
-      {
-        currentPage === '/'
-          ? <Main />
-          : currentPage === '/about'
-            ? <About />
-            : <NotFound />
-      }
+      <BrowserRouter>
+        <nav>
+          <Link to='/'>Home Page</Link>
+          <Link to='/about'>About App</Link>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
