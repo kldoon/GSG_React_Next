0db78fa5142906919e56f2afaadd391d64255388
Khaldoun Halawani <khalawani@sadasol.com>
2025-01-08 20:05:31 +0200

    Routeing 5 Query Params (Student App 17)

--- 1_Students App/src/screens/Main.screen.tsx
+++ 1_Students App/src/screens/Main.screen.tsx
@@ -3,20 +3,31 @@ import AddForm from "../components/add-form/add-form.component";
 import Student from "../components/student/student.component";
 import useLocalStorage from "../hooks/local-storage.hook";
 import { IStudent } from "../types";
+import { useSearchParams } from "react-router-dom";
 
 const Main = () => {
   const [studentsList, setStudentsList] = useState<IStudent[]>([]);
+  const [filteredList, setFilteredList] = useState<IStudent[]>(studentsList);
   const [totalAbsents, setTotalAbsents] = useState(0);
   const lastStdRef = useRef<HTMLDivElement>(null);
+  const [params, setParams] = useSearchParams();
 
   const { storedData } = useLocalStorage(studentsList, 'students-list');
-
   useEffect(() => {
     const stdList: IStudent[] = storedData || [];
     const totalAbs = stdList.reduce((prev, cur) => { return prev + cur.absents }, 0);
     setTotalAbsents(totalAbs);
     setStudentsList(stdList);
   }, [storedData]);
+
+  useEffect(() => {
+    const query = params.get('q') || '';
+    if (query) {
+      setFilteredList(studentsList.filter(std => std.name.toLowerCase().includes(query.toLowerCase())));
+    } else {
+      setFilteredList(studentsList);
+    }
+  }, [params, studentsList]);
 
   const removeFirst = () => {
     const newList = [...studentsList];
@@ -39,6 +50,12 @@ const Main = () => {
     }
   }
 
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+    params.set('q', query);
+    setParams(params);
+  }
+
   return (
     <>
       <AddForm className="addForm" onSubmit={handleAddStudent} />
@@ -47,8 +64,11 @@ const Main = () => {
         <button onClick={scrollToLast}>Scroll to Last</button>
         <b style={{ fontSize: '12px', fontWeight: 100, color: 'gray' }}>Total Absents {totalAbsents}</b>
       </div>
+      <div className='filter'>
+        <input type="text" placeholder="Search" onChange={handleSearch} value={params.get('q') || ''} />
+      </div>
       {
-        studentsList.map(student => (
+        filteredList.map(student => (
           <Student
             key={student.id}
             id={student.id}
