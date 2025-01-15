5c3c512b13b4aec9712775e449c4dcd1eb958279
Khaldoun Halawani <khalawani@sadasol.com>
2025-01-15 19:00:03 +0200

    Context API, logout (Student App 24)

--- 1_Students App/src/App.tsx
+++ 1_Students App/src/App.tsx
@@ -2,21 +2,20 @@ import './App.css'
 import Main from './screens/Main.screen';
 import About from './screens/About.screen';
 import NotFound from './screens/NotFound.screen';
-import { Link, Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import StudentDetails from './screens/StudentDetails.screen';
 import { useState, useEffect } from 'react';
 import useLocalStorage from './hooks/local-storage.hook';
 import { IStudent } from './types';
 import AddStudent from './screens/AddStudent.screen';
-import { AuthProvider } from './providers/authProvider';
 import Login from './screens/Login.screen';
+import NavBar from './components/nav-bar/nav-bar.component';
 
 function App() {
   const h1Style = { color: '#69247C', fontSize: '24px' };
 
   const [studentsList, setStudentsList] = useState<IStudent[]>([]);
   const [totalAbsents, setTotalAbsents] = useState(0);
-  const location = useLocation();
 
   const { storedData } = useLocalStorage(studentsList, 'students-list');
 
@@ -43,32 +42,26 @@ function App() {
   }
 
   return (
-    <AuthProvider>
-      <div className="main wrapper">
-        <h1 style={h1Style}>Welcome to GSG React/Next Course</h1>
-        <nav>
-          <Link to='/' className={location.pathname === '/' ? 'active' : ''}>Home Page</Link>
-          <Link to='/add' className={location.pathname === '/add' ? 'active' : ''}>Add Student</Link>
-          <Link to='/about' className={location.pathname === '/about' ? 'active' : ''}>About App</Link>
-        </nav>
-        <Routes>
-          <Route path='/'
-            element={
-              <Main
-                studentsList={studentsList}
-                totalAbsents={totalAbsents}
-                onAbsent={handleAbsentChange}
-                onRemove={removeFirst}
-              />
-            } />
-          <Route path='/add' element={<AddStudent onAdd={handleAddStudent} />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/student/:id' element={<StudentDetails />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </div>
-    </AuthProvider>
+    <div className="main wrapper">
+      <h1 style={h1Style}>Welcome to GSG React/Next Course</h1>
+      <NavBar />
+      <Routes>
+        <Route path='/'
+          element={
+            <Main
+              studentsList={studentsList}
+              totalAbsents={totalAbsents}
+              onAbsent={handleAbsentChange}
+              onRemove={removeFirst}
+            />
+          } />
+        <Route path='/add' element={<AddStudent onAdd={handleAddStudent} />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/student/:id' element={<StudentDetails />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </div>
   )
 }
 
