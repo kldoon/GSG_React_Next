9b9a219c4b6c05ba2c0014bbf65c4dd2f06ca4d4
Khaldoun Halawani <khalawani@sadasol.com>
2025-02-01 12:13:28 +0200

    NextJS 1 (initial project)

---
+++ 3_next-example/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from './page.module.css';
+
+const Home = () => {
+  return (
+    <div className={styles.page}>
+      <h1>Hello Next JS</h1>
+      <p>This is is the first next js app</p>
+    </div>
+  );
+}
+
+export default Home;
