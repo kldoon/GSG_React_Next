361cb4f9ce25a1b8e36b7ce4ecfd6cf22d98b7ca
Khaldoun Halawani <khalawani@sadasol.com>
2025-02-12 20:03:27 +0200

    NextJS 7 (Fonts-1)

--- 4_news_app/components/latest-news/item/Item.tsx
+++ 4_news_app/components/latest-news/item/Item.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import classes from '../latest-news.module.css';
 import mockImg from '@/public/n1.jpg';
+import { Barriecito } from 'next/font/google';
 
 interface IProps {
   isHighlighted: boolean;
 }
+
+const barriecitoFont = Barriecito({ weight: '400', subsets: ['latin'] });
+
 const Item = (props: IProps) => {
   return (
     <div className={`${classes.newsItem} ${props.isHighlighted ? classes.highlighted : ''}`}>
       <div className={classes.info}>
-        <h3>Urban Planning</h3>
+        <h3 style={{ ...barriecitoFont.style }}>Urban Planning</h3>
         <p>Explore the recent developments in urban planning as cities expand and adapt to modern challenges.</p>
         <div className="button outline">Read More</div>
       </div>
