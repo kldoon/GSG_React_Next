30a8c2e1d47b6fa3ed2b430eef0794063368555f
Khaldoun Halawani <khalawani@sadasol.com>
2025-03-15 23:01:24 +0200

    NextJS 22: Minor fix

--- 4_news_app/app/(main)/admin/page.tsx
+++ 4_news_app/app/(main)/admin/page.tsx
@@ -1,15 +1,17 @@
 'use client';
 import { redirect } from 'next/navigation';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { toast } from 'react-toastify';
 
 const Page = () => {
-  const user = JSON.parse(localStorage.getItem('auth-user') || '{}');
-  const token = localStorage.getItem('auth-token');
-  if (!token || !user || user.role !== 'admin') {
-    toast.warn("You don't have the permission to access this page!");
-    redirect('/user/login');
-  }
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('auth-user') || '{}');
+    const token = localStorage.getItem('auth-token');
+    if (!token || !user || user.role !== 'admin') {
+      toast.warn("You don't have the permission to access this page!");
+      redirect('/user/login');
+    }
+  }, []);
 
   return (
     <div>
