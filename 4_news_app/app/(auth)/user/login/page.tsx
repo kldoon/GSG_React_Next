616854d4c089ed9d930721ae2d22192cc7b8aa0b
Khaldoun Halawani <khalawani@sadasol.com>
2025-03-22 10:56:50 +0200

    NextJS 25: Auth enhancements

--- 4_news_app/app/(auth)/user/login/page.tsx
+++ 4_news_app/app/(auth)/user/login/page.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
 import classes from "./login.module.css";
 import LoginForm from '@/components/auth/login-form/LoginForm';
 
-const Page = () => {
+interface IProps {
+  searchParams: Promise<{ msg: string }>;
+}
+
+const Page = async (props: IProps) => {
+  const { msg } = (await props.searchParams);
+
   return (
     <div className={classes.loginContainer}>
+      <p style={{ color: 'red' }}>{msg}</p>
       <h1 className={classes.loginTitle}>Welcome Back</h1>
       <p className={classes.loginDescription}>Please enter your credentials to login</p>
       <LoginForm />
--- 4_news_app/middleware.ts
+++ 4_news_app/middleware.ts
@@ -2,6 +2,25 @@ import { NextRequest, NextResponse } from "next/server";
 import { verifyToken } from "./utils/auth";
 import { cookies } from "next/headers";
 
+const authenticateUser = async (token: string | undefined, request: NextRequest) => {
+
+  // verify there is a token
+  if (!token) {
+    // else redirect to login page!
+    return NextResponse.redirect(new URL(`/user/login?msg=${encodeURIComponent('You must be logged in to access this!')}`, request.url));
+  }
+
+  // verify the token is valid
+  const user = await verifyToken(token);
+
+  if (!user) {
+    // else redirect to login page!
+    (await cookies()).delete('auth-token');
+    return NextResponse.redirect(new URL(`/user/login?msg=${encodeURIComponent('Your session is expired!')}`, request.url));
+  }
+  return user;
+}
+
 export const middleware = async (request: NextRequest) => {
   // using the middle for logging
   console.log(`A request is made to ${request.nextUrl.pathname}`);
@@ -10,28 +29,29 @@ export const middleware = async (request: NextRequest) => {
 
   switch (request.nextUrl.pathname) {
     case '/admin':
-      // verify there is a token
-      if (!token) {
-        // else redirect to login page!
-        return NextResponse.redirect(new URL('/user/login', request.url));
-      }
-
-      // verify the token is valid
-      const user = await verifyToken(token);
-
-      if (!user) {
-        // else redirect to login page!
-        (await cookies()).delete('auth-token');
-        return NextResponse.redirect(new URL('/user/login', request.url));
+      const res = await authenticateUser(token, request);
+      if (res instanceof (NextResponse)) {
+        return res;
       }
 
       // check the role stored the in token
-      if (user.role !== 'admin') {
+      if (res.role !== 'admin') {
         // else redirect to home page!
         return NextResponse.redirect(new URL('/', request.url));
       }
       // if role is admin => move to next page (admin page)
       break;
+    case '/add-news': {
+      const res = await authenticateUser(token, request) as News.IUser;
+      if (res instanceof (NextResponse)) {
+        return res;
+      }
+
+      if (!['editor', 'admin'].includes(res.role)) {
+        return NextResponse.redirect(new URL('/', request.url));
+      }
+      break;
+    }
 
     default:
       break;
@@ -42,5 +62,5 @@ export const middleware = async (request: NextRequest) => {
 }
 
 export const config = {
-  matcher: ['/news/:path*', '/admin/:path*']
+  matcher: ['/news/:path*', '/admin/:path*', '/:path*']
 }
